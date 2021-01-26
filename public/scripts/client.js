2f3f76488d2cb895bfe92b95d7b3cea0d49c4c82
alexhauka <alexhauka@gmail.com>
2021-01-26 23:36:18 +0000

    add dynamic tweets plugged in with markup in client.js, hardcode data placeholder

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // Fake data taken from initial-tweets.json => REMOVE AFTER IMPLEMENTING AJAX GET REQUEST
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+$(document).ready(function() {
+  
+  // UPDATE
+
+  const renderTweets = function(tweets) {
+    // const data = tweets
+
+    // loops through tweets
+    for (const tweet of data) {
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      $('.container').append(createTweetElement(tweet));
+    }
+  }
+  
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <section class="tweet-feed">
+      <article>
+
+        <header>
+          <div class="user-info">
+            <img src=${tweet.user.avatars}>
+            <label>${tweet.user.name}</label>
+          </div>
+          <div class="handle">
+            <label>${tweet.user.handle}</label>
+          </div>
+        </header>
+
+        <section>
+          <p>${tweet.content.text}</p>
+        </section>
+
+        <footer>
+          <label>${tweet.created_at}</label>
+          <label>buttons</label>
+        </footer>
+
+      </article>
+    </section>
+    `;
+    return $tweet;
+  }
+
+  renderTweets(data);
+  
+});
